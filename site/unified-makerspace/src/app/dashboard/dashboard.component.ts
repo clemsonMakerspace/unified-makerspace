08b9e8e584080702a5af2d2aced6913325079e80
Mason Fox <mgfox@clemson.edu>
2021-02-18 14:23:18 -0500

    exporting users and tasks

--- site/unified-makerspace/src/app/dashboard/dashboard.component.ts
+++ site/unified-makerspace/src/app/dashboard/dashboard.component.ts
@@ -147,5 +147,32 @@ export class DashboardComponent implements OnInit {
     }
   }
 
+  // exporting users data to csv file
+  exportUserData(){
+    let rowDelimiter = '\n'
+    let columnDelimiter = ','
+    let formattedData = 'data:text/csv;charset=utf-8,'
+
+    //setup header of csv as All Users, New Users, Day
+    formattedData += "Day" + columnDelimiter
+    this.data.forEach(function (item, index) {
+      formattedData += item.name + columnDelimiter
+    });
+    formattedData = formattedData.slice(0,-1) + rowDelimiter //replace last comma with newline
+
+    let temp = this.data[1]
+    //for each day listed in the series, record data
+    this.data[0].series.forEach(function(item, index) {
+      formattedData += item.name + columnDelimiter + item.value + columnDelimiter + temp.series[index].value + rowDelimiter 
+    });
+
+    //Download data as a csv
+    let encodedUri = encodeURI(formattedData);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "Users.csv")
+    document.body.appendChild(link)
+    link.click()
+  }
 
 }
--- site/unified-makerspace/src/app/dashboard/tasks/tasks.component.ts
+++ site/unified-makerspace/src/app/dashboard/tasks/tasks.component.ts
@@ -38,4 +38,26 @@ export class TasksComponent implements OnInit {
     );
 
   }
+
+  exportTaskData(){
+    let rowDelimiter = '\n'
+    let columnDelimiter = ','
+    let formattedData = 'data:text/csv;charset=utf-8,'
+
+    //setup header of csv as Task, Person, Status
+    formattedData += "Task" + columnDelimiter + "Person" + columnDelimiter + "Status" + rowDelimiter
+
+    //loop through tasks and add data to csv
+    this.tasks.forEach(function (item, index) {
+      formattedData += item.task + columnDelimiter + item.person + columnDelimiter + item.status + rowDelimiter
+    });
+
+    //Download data as a csv
+    let encodedUri = encodeURI(formattedData);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "Tasks.csv")
+    document.body.appendChild(link)
+    link.click()
+  }
 }
