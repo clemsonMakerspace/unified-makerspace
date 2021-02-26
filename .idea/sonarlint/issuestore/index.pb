ecf5e8026e8a6b724e1128242382f225c4e16aaf
aru-py <arunorth@gmail.com>
2021-02-26 11:19:57 -0500

    improved documentation

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,22 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+api/docs/make.bat,0/7/07dc4a8032fa07edc04789498ecbfcad4f7cf341
+N
+api/docs/build/html/index.html,d/1/d182bbef46180714b73c26501f2b5c42df9c0995
+R
+"api/docs/source/authentication.rst,8/9/896f9828305302f4d6cf80060a2624eb070e4719
+I
+api/docs/source/index.rst,1/e/1e9804eb947900c1ac9f309061c7febf92be4e91
+I
+api/docs/source/users.rst,9/6/96f30035685eeefa7d04336df38ea87ba0410bf7
+I
+api/docs/source/tasks.rst,b/c/bcb2b827d08e02c9b6aae4a06872f2a6f106fdb2
+G
+api/docs/source/conf.py,3/e/3ebb1310b3fa303d76ec74ddbbf1fde8769706ec
+;
+api/auth.py,6/4/6430257701ab448143b0356d8d0d32b3537b19f2
+:
+
+api/app.py,2/b/2b829e25c7d72bb6b04ca84f1764732278d2c9cf
--- api/auth.py
+++ api/auth.py
@@ -1,30 +1,45 @@
+import flask
+
+app = flask.Flask('__main__')
+
+def get_users():
+    pass
+
+
+@app.route('/users/')
 def create_user(email: str, password: str, role: str):
     """
     Creates a new user in the Cognito Pool.
 
+    ================   ============
+    **Endpoint**        /api/users/create
+    **Request Type**    POST
+    **Access**          ALL
+    ================   ============
+
+
     Parameters
     -----------
-    email : str
+    email : str, required
         The email of the user.
-    password : str
+    password : str, required
         The raw password of the user.
-    role : str
+    role : str, required
         Specified role of the user.
 
     Returns
     --------
+    UserCreationSuccess : Response
     code
-        200
-    token : str
-       32-bit authentication token.
+       200
+    message
+        The user has been successfully created.
 
-
-    Raises
-    -------
+    EmailInUse : Response
     code
-        400
-    message: str
-        The email is already in use.
+       400
+    message
+        This email is already being used.
     """
 
 
@@ -32,18 +47,31 @@ def delete_user(email: str, auth_token: str):
     """
     Deletes a user specified by their email.
 
+    ================   ============
+    **Endpoint**        /api/users/delete
+    **Request Type**    POST
+    **Access**          MANAGER
+    ================   ============
+
     Parameters
     -----------
-    email : str
+    email : str, required
         The email of the user.
-    auth_token : str
-        Token to very user credentials.
+    auth_token : str, required
+        Token to verify user credentials.
 
     Returns
     --------
+    SuccessfulDelete : Response
     code
        200
     message
-       The user has been successfully deleted.
+        Success
+
+    FailedDelete : Response
+    code
+       404
+    message
+        Unauthorized.
 
     """
