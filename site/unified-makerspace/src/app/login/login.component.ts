7e577a8e2893e108e082bc516af2cf5df800d946
aru-py <arunorth@gmail.com>
2021-02-26 10:17:11 -0500

    Revert "cognito login"
    This reverts commit 92802f37e9a514f1d413cf1a08bef67fb529162a.

--- site/unified-makerspace/src/app/login/login.component.ts
+++ site/unified-makerspace/src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../shared/auth/auth.service';
 import {Router} from '@angular/router';
-import {Auth} from 'aws-amplify'
 
 @Component({
   selector: 'app-login',
@@ -15,8 +14,8 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   registerForm: FormGroup;
-  verificationForm: FormGroup;
-  toVerifyEmail = false;
+
+
 
   // can either be login or register
   formType = 'login';
@@ -38,10 +37,7 @@ export class LoginComponent implements OnInit {
       'password': new FormControl('password', Validators.required),
       'confirmPassword': new FormControl('', Validators.required)
     })
-    
-    this.verificationForm = new FormGroup({
-      'code': new FormControl('ABCDEF', Validators.required)
-    })
+
 
   }
 
@@ -52,61 +48,19 @@ export class LoginComponent implements OnInit {
       .then()
 
   }
-  
+
   login() {
     // todo check for success
     this.auth.login();
     this.router.navigate(['dashboard']).then()
-
-    //Todo: test sign in with an existing user
-    const user = {
-      username: this.loginForm.get('username').value,
-      password: this.loginForm.get('password').value
-   }
-
-    Auth.signIn(user).then(user => {
-      console.log(user);
-    })
-      .catch(err => console.log(err));
-
   }
-  
 
   registerSuccess = false;
   register() {
     this.auth.register();
     this.registerSuccess = true;
-
-    //Add first and last name entries
-    const user = {
-      username: this.registerForm.get('username').value,
-      password: this.registerForm.get('password').value,
-      attributes: {
-           email: this.registerForm.get('username').value
-           //phone_number
-           // other custom attributes
-         }
-   }
-
-
-    Auth.signUp(user)
-    .then(data => {
-      console.log(data);
-      this.toVerifyEmail = true;
-      this.formType = 'register';
-    })
-    .catch(err => console.log(err));
-  
+    // todo send confirmation email
   }
 
-  verify(){
-    Auth.confirmSignUp(this.registerForm.get('username').value, this.verificationForm.get('code').value, 
-      {forceAliasCreation: true}).then(data => {
-            console.log(data)
-            this.toVerifyEmail = false;
-            this.formType = 'login'
-            this.switchMode()
-         })
-           .catch(err => console.log(err));
-  }
+
 }
