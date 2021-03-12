7ab5aba63585369f107b535fe660aaf5a580d798
Mason Fox <mgfox@clemson.edu>
2021-03-11 21:32:33 -0500

    updated create use lambda

--- aws_lambda/CreateUser.py
+++ aws_lambda/CreateUser.py
@@ -4,46 +4,58 @@ import boto3
 clientID = "20nnrq12vp19a99c58g2r0b0og" 
 
 def CreateUserHandler(event, context):
-    
     try:
         username = event['username']
         password = event["password"]
-        role = event['role']
+        cardID = event['cardID']
         email = event['email']
         first_name = event["first"]
         last_name = event["last"]
     except:
         return {
-                'statusCode': 400,
+                'statusCode': 401,
                 'body': json.dumps({
                     'Message' : 'Error loading data. '
                  })
         }
         
+    #AWS resources
     client = boto3.client('cognito-idp')
+    
+    db = boto3.resource('dynamodb')
+    table = db.Table('MakerspaceUser')
+    
     custom_attributes = [
         {'Name' : 'email', 'Value': email},
         {'Name' : 'custom:firstname', 'Value': first_name}, 
-        {'Name' : 'custom:lastname', 'Value': last_name}, 
-        {'Name' : 'custom:role', 'Value': role}
+        {'Name' : 'custom:lastname', 'Value': last_name}
         ]
 
     try:
+        #Create new user in cognito pool
         response = client.sign_up(ClientId=clientID, Username=username, Password=password, UserAttributes=custom_attributes)
     except client.exceptions.UsernameExistsException as e:
+        #Return error if email is already in use
         return {
-                'statusCode': 400,
-                'body': json.dumps({
-                    'Message' : 'This email is already being used. '
-                 })
+                'code': 400,
+                'message' : 'This email is already being used. '
         }
-        
-        
-    if response['UserConfirmed'] == True:
+    except Exception as e:
         return {
-                'statusCode': 200,
-                'body': json.dumps({
-                    'Message' : 'The user has been successfully created. '
-                 })
+                'code': 402,
+                'message' : e
         }
+    
+    #Get token for new user
+    auth_token = response['UserSub']
+   
+    #Store user data in databse
+    new_user = {'PK':auth_token, 'SK':"Profile", 'AccessLevel':"student", 'Email':email, 'First_Name':first_name, 'Last_Name':last_name, 'cardID':cardID}
+    table.put_item(Item=new_user)
+    
+    return {
+            'code': 200,
+            'message': 'The user has been successfully created. ',
+             'auth_token':auth_token
+    }
         
