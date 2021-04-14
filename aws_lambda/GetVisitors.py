40529d728f7b68bda61c7a4ed000b554a04aa2c9
Matt Franchi <mattwfranc@gmail.com>
2021-04-14 18:57:47 -0400

    GetVisitors -> no need for Visitors scan anymore

--- aws_lambda/GetVisitors.py
+++ aws_lambda/GetVisitors.py
@@ -22,19 +22,15 @@ def GetVisitors(body):
     start_time = body["start_time"]
     end_time = body["end_time"]
 
-    visitors = Visitors.scan()
-    visitors_list = visitors["Items"]
-
     visits = Visits.scan()
     visits_list = visits["Items"]
 
     visits_in_tf = []
 
-    for visitor in visitors_list:
-        for visit in visits_list:
-            if visit["visitor_id"] == visitor["visitor_id"]:
-                if visit["sign_in_time"] >= start_time and visit["sign_out_time"] <= end_time:
-                    visits_in_tf.append(visit)
+
+    for visit in visits_list:
+        if visit["sign_in_time"] >= start_time and visit["sign_out_time"] <= end_time:
+            visits_in_tf.append(visit)
 
     return visits_in_tf
 
