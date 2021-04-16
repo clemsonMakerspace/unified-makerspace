f94e2e5efac04b79836b2112397d390f1fd16667
Matt Franchi <mattwfranc@gmail.com>
2021-04-16 12:28:41 -0400

    added GetVisitors lambda test

--- tests/visitors.py
+++ tests/visitors.py
@@ -1,6 +1,7 @@
 import requests as r
 
 url = "https://9bhfui3vn2.execute-api.us-east-1.amazonaws.com/api/visitors"
+# CREATE VISITOR
 res = r.put(url, json = {
 
     "hardware_id": "902100",
@@ -13,5 +14,12 @@ res = r.put(url, json = {
         "major": "Computer Science",
         "password": "password"
     }
+})
+print(res.json())
+
+# GET VISITORS
+res = r.post(url, json = {
+    "start_time": 0,
+    "end_time": 9999999999
 })
 print(res.json())
