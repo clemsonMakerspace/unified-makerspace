9d2cb712eb3238bc4906fead362d19691ab62d79
Matt Franchi <mattwfranc@gmail.com>
2021-04-16 18:05:42 -0400

    Login.py -> added CORS headers

--- aws_lambda/Login.py
+++ aws_lambda/Login.py
@@ -75,8 +75,12 @@ def loginHandler(event, context):
             return {
                 'statusCode': 200,
                 'headers': {
-                    'Content-Type': 'text/plain'
-                },
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+
+            },
                 'body': json.dumps(str(result))
             }
             
