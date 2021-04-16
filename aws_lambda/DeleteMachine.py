f4345e7abd1584f1c534ecffcfd1f3b63a1b752a
Matt Franchi <mattwfranc@gmail.com>
2021-04-16 18:02:01 -0400

    DeleteMachine.py -> added CORS headers

--- aws_lambda/DeleteMachine.py
+++ aws_lambda/DeleteMachine.py
@@ -56,7 +56,11 @@ def DeleteMachineHandler(event, context):
         return {
             'statusCode': 500,
             'headers': {
-                'Content-Type': 'text/plain'
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+
             },
             'body': json.dumps({
                 'Message': str(e)
