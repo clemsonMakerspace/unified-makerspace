4d527b70364e0275cbc6765ad438703bfdedacb9
Ramnek Chhatwal <ramychha@charter.net>
2021-04-20 14:01:41 -0400

    final iteration

--- iot_core/SignOut/start.sh
+++ iot_core/SignOut/start.sh
@@ -2,7 +2,7 @@
 # stop script on error
 set -e
 
-Check to see if root CA file exists, download if not
+# Check to see if root CA file exists, download if not
 if [ ! -f ../certs/root-CA.crt ]; then
   printf "\nDownloading AWS IoT Root CA certificate from AWS...\n"
   curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > root-CA.crt
