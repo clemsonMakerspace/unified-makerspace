622f86d144d990a848724d3bf615370c6c049bd3
Ramnek Chhatwal <ramychha@charter.net>
2021-04-20 13:58:29 -0400

    additional comments removed

--- iot_core/SignOut/start.sh
+++ iot_core/SignOut/start.sh
@@ -20,8 +20,5 @@ LOCATION="Watt_Center"
 EVENT="SignOut"
 HAT_CONNECTED="True"
 
-# run pub/sub sample app using certificates downloaded in package
-#printf "\nRunning pub/sub sample application...\n"
-#python aws-iot-device-sdk-python/samples/basicPubSub/basicPubSub.py -e a5oyk3iuhy30n-ats.iot.us-east-1.amazonaws.com -r root-CA.crt -c CUmakeit_01.cert.pem -k CUmakeit_01.private.key
-
-python signout.py --endpoint $THING_ENDPOINT --root-ca $CA_CERT --cert $DEVICE_CERT --key $PRIVATE_KEY --location $LOCATION --event $EVENT --topic $TOPIC --hat-connected $HAT_CONNECTED
+python signout.py --endpoint $THING_ENDPOINT --root-ca $CA_CERT --cert $DEVICE_CERT --key $PRIVATE_KEY --location 
+$LOCATION --event $EVENT --topic $TOPIC --hat-connected $HAT_CONNECTED
