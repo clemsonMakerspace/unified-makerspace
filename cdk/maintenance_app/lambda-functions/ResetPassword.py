c4e2e0f74bde58e257969c3fa11b337c8c0fb315
Matt Franchi <mattwfranc@gmail.com>
2021-04-29 10:54:10 -0400

    ResetPassword.py -> CORS

--- cdk/maintenance_app/lambda-functions/ResetPassword.py
+++ cdk/maintenance_app/lambda-functions/ResetPassword.py
@@ -6,22 +6,34 @@ client = boto3.client('cognito-idp')
 clientID = "20nnrq12vp19a99c58g2r0b0og"
 poolID = "us-east-1_l5xLuC13j"
 
+
 def ResetPasswordHandler(event, context):
-    # TODO implement
-    try: 
-        data = json.loads(event["body"])
-        username = data['email']
-        
-        client.admin_reset_user_password({
-            UserPoolId=poolID,
-            Username=username
-        })
+    try:
+        username = event["email"]
+
+        client.forgot_password(ClientId=clientID, Username=username)
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+
+            },
+            'message': json.dumps(str("Reset password email sent."))
+        }
     except Exception as e:
         # Return exception with response
         return {
             'statusCode': 500,
-            'body': json.dumps({
-                'Message': str(e)
-            })
+            'headers': {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+
+            },
+            'message': json.dumps(str(e))
         }
 
