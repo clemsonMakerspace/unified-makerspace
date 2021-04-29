dfa05448fa0ccd9a64829d79f8778d19422f8729
Matt Franchi <mattwfranc@gmail.com>
2021-04-29 13:21:32 -0400

    GetMachinesStatus.py -> adjusting for frontend using ms-based epoch time

--- cdk/maintenance_app/lambda-functions/GetMachinesStatus.py
+++ cdk/maintenance_app/lambda-functions/GetMachinesStatus.py
@@ -2,12 +2,14 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key
 import decimal
+import time
 
 dynamodb = boto3.resource('dynamodb')
 dynamodb_client = boto3.client('dynamodb')
 
 Machines = dynamodb.Table('Machines')
 Tasks = dynamodb.Table('Tasks')
+
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -17,14 +19,17 @@ class DecimalEncoder(json.JSONEncoder):
             return list(obj)
         return super(DecimalEncoder, self).default(obj)
 
+
 # get machine data given an id
 def GetMachines():
     machines = Machines.scan()
     return machines["Items"]
 
+
 def GetTasks():
     tasks = Tasks.scan()
     return tasks["Items"]
+
 
 def processAvailability(machines, tasks, start_time, end_time):
     avail = {}
@@ -32,8 +37,14 @@ def processAvailability(machines, tasks, start_time, end_time):
         times = []
         for task in tasks:
             if (task["tags"])[0] == machine["machine_name"]:
-                if task["date_created"] >= start_time and task["date_completed"] <= end_time:
-                    times.append((task["date_resolved"],task["date_resolved"]))
+
+                if int(task["date_created"]) >= int(start_time) and int(task["date_created"]) <= int(end_time):
+                    print("true2")
+                    if int(task["date_resolved"]) == 0:
+                        times.append((int(task["date_created"]), int(time.time())))
+                    else:
+                        times.append((int(task["date_created"]), int(task["date_resolved"])))
+        times.sort()
         avail[machine["machine_name"]] = times
     return avail
 
@@ -46,21 +57,23 @@ def GetMachineStatusHandler(event, context):
     tasks = list(GetTasks())
 
     body = json.loads(event["body"])
-    start_time = body["start_date"]
-    end_time = body["end_date"]
 
-    availability = processAvailability(machines,tasks,start_time, end_time)
+    start_time = int(body["start_date"]) / 1000
+    end_time = int(body["end_date"]) / 1000
+    print(start_time)
+    print(end_time)
 
+    availability = processAvailability(machines, tasks, start_time, end_time)
 
     return {
         'statusCode': 200,
         'headers': {
-                'Content-Type': 'text/plain',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-                },
-        'body': json.dumps({'machines': availability},cls=DecimalEncoder)
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps({'machines': availability}, cls=DecimalEncoder)
     }
 
 
