9256b2de32a1b9e3171a9f27cbb63ddbcdd31000
Matt Franchi <mattwfranc@gmail.com>
2021-04-29 10:16:50 -0400

    GenerateUserToken.py -> fixed body response

--- cdk/maintenance_app/lambda-functions/GenerateUserToken.py
+++ cdk/maintenance_app/lambda-functions/GenerateUserToken.py
@@ -1,6 +1,6 @@
 import json
 from random import randint
-import boto3 
+import boto3
 import os
 from base64 import b64encode
 import time
@@ -14,33 +14,41 @@ Users = dynamodb.Table('Users')
 
 def GenerateUserTokenHandler(event, context):
     try:
-        #generating unique visit id
+        # generating unique visit id
         user_token = b64encode(os.urandom(5)).decode('utf-8')
-        
-        #TODO 
-        #Add user token into some table
-        #Record and store time into table as well
+
+        # TODO
+        # Add user token into some table
+        # Record and store time into table as well
         insertToken = dynamodb.Table('userVerificationToken')
         tokenEpochTime = int(time.time())
-        
-        input = {'generatedToken' : user_token, 
-                 'tokenTime' : tokenEpochTime
-                }
-                
-        response = insertToken.put_item(Item = input)
-        
+
+        input = {'generatedToken': user_token,
+                 'tokenTime': tokenEpochTime
+                 }
+
+        response = insertToken.put_item(Item=input)
+
         return {
             'statusCode': 200,
-            'body': json.dumps({
-                'Message': str(user_token)
-            })
+            'headers': {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps({'user_token': str(user_token)})
         }
-        
+
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps({
-                'Message': str(e)
-            })
+            'headers': {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'message': json.dumps(str(e))
         }
-        
+
