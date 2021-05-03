d7f066ac16ad1eb7f8e57803b23ce9119670d748
summerslake <lakes@g.clemson.edu>
2021-05-03 17:13:01 -0400

    Done.

--- site/unified-makerspace/src/environments/environment.prod.ts
+++ site/unified-makerspace/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  server: 'https://bvs5vq23ji.execute-api.us-east-1.amazonaws.com'
+  server: 'https://ochf3ex827.execute-api.us-east-1.amazonaws.com'
 };
