9035cc94cc664ce56857dc452efe4ce0ccfb1c60
Weimin Li <weiminl@clemson.edu>
2021-11-16 14:06:54 -0500

    add background and logo

--- site/visitor-console/src/index.tsx
+++ site/visitor-console/src/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.scss';
 import App from './components/App/App';
+import bg from './asset/background.webp'
+import logo from './asset/makerspace_logo.webp'
 
 document.body.className="bg-dark";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App></App>
+    <div style={{backgroundImage: `url(${bg})`, height: "100%"}}>
+      <img src={logo} />
+      <App></App>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
