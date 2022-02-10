e73581a20ffde02fd87f00e2abd244bbfd52be7a
Joshua Little <>
2022-02-10 11:17:50 -0500

    Updated footer text

--- site/visitor-console/src/components/Footer.tsx
+++ site/visitor-console/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return <div className="text-muted w-full mt-5 fs-6 text-center">
-        This site is an open-source AWS Capstone project.
+        This site is developed by students in Clemson's School of Computing Capstone.
         <br />
         <a className="link-secondary text-muted" href="https://github.com/clemsonMakerspace/unified-makerspace/tree/mainline/site/visitor-console">
             Contribute to the code on GitHub
