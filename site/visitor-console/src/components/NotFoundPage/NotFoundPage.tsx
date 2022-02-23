364d8e8b963fc9b10f2e4b722c882ff867f43dd9
mikebrandin1 <mikejbrandin@gmail.com>
2022-02-22 21:16:46 -0500

    Update NotFoundPage.tsx

--- site/visitor-console/src/components/NotFoundPage/NotFoundPage.tsx
+++ site/visitor-console/src/components/NotFoundPage/NotFoundPage.tsx
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 
 class NotFoundPage extends React.Component{
     render(){
-        return <div>
+        return <div className="container bg-primary p-5 rounded" style={{height: "400px"}}>
             <p style={{textAlign:"center", color:"white", paddingBottom: "100px"}}>
-                <h1 style={{fontSize:"350%"}}>Error 404</h1>
-                <h2>Page Not Found</h2>    
+                <div className="text-center mb-4">
+
+                    <h1 className="text-secondary fw-bold mb-1 text-center" style={{fontSize:"350%"}}>ERROR 404 :(</h1>
+                    <h2 className="text-white fw-bold mb-1 text-center">Page Not Found</h2>
+                </div>
+    
             </p>
-            <p style={{textAlign:"center"}}>
-                <Link to="/" style={{color:"white", paddingBottom: "100px", fontSize:"200%"}}>Go Back to Home Page</Link>
-            </p>
-            
+            <div className="text-secondary fw-bold mb-4 text-center">
+                <p style={{textAlign:"center"}}>
+                    <Link to="/" style={{color:"white", paddingBottom: "50px", fontSize:"150%"}}>Go Back to Home Page</Link>
+                </p>
+            </div>
+
           </div>;
     }
 }
