4e9c21224a2a5df242c5830ff0e46241a62e820e
Joshua <Jplittle2001@gmail.com>
2022-03-02 00:55:19 -0500

    Replaced CRA with Vite and Exposed Build Env Variable for API (#79)
    * Replace CRA with Vite
    
    * Adjust frontend to accept environment variable
    
    * Updated cdk build of visitor frontend

--- site/visitor-console/src/main.tsx
+++ site/visitor-console/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "./styles/style.scss";
+import "./styles/global.scss";
 import App from "./pages/App";
 import background_image from "./assets/background.webp";
 import logo from "./assets/makerspace_logo.webp";
