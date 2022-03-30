2da547693a89592280729dfcb27ff2c2b3ef6e1d
Jonathan Daniel <gcagrad19@gmail.com>
2022-03-29 23:41:25 -0400

    Finished Script v1

--- cdk/visit/migration_scripts/migrate_data_to_2_tables.py
+++ cdk/visit/migration_scripts/migrate_data_to_2_tables.py
@@ -6,10 +6,15 @@ new tables.
 import boto3
 import os
 
-# TODO: Update these to point to correct env variables.
-original_table_name = os.environ["TABLE_NAME"]
-visits_table_name = os.environ["VISITS_TABLE_NAME"]
-users_table_name = os.environ["USERS_TABLE_NAME"]
+
+def generate_role_arn() -> str:
+    """
+    Generate a role arn for the migration script.
+
+    Returns:
+        A role arn.
+    """
+    return 'arn:aws:iam::{}:role/{}'.format(os.environ['AWS_ACCOUNT_ID'], 'migration-role')
 
 
 def get_table(table_name: str) -> boto3.resources.base.ServiceResource:
@@ -43,6 +48,15 @@ def get_all_data(table: boto3.resources.base.ServiceResource) -> list:
 
 # This part runs the migration
 if __name__ == '__main__':
+    client = boto3.client('sts')
+    response = client.assume_role(
+        RoleArn=generate_role_arn(),
+        RoleSessionName="data_migration_session")
+
+    original_table_name = os.environ["ORIGINAL_TABLE_NAME"]
+    visits_table_name = os.environ["VISITS_TABLE_NAME"]
+    users_table_name = os.environ["USERS_TABLE_NAME"]
+
     original_table = get_table(original_table_name)
     visits_table = get_table(visits_table_name)
     users_table = get_table(users_table_name)
@@ -53,9 +67,18 @@ if __name__ == '__main__':
     # Iterate over the table. If PK is a timestamp, this is a visit
     for row in original_data:
         if row['PK'].isdigit():
-            visits_table.put_item(
-                Item={'visit_time': row['PK'], 'username': row['SK'], 'location': row['location']})
+            if 'location' in row:
+                visits_table.put_item(
+                    Item={'visit_time': int(row['PK']), 'username': row['SK'], 'location': row['location']})
+            else:
+                visits_table.put_item(
+                    Item={'visit_time': int(row['PK']), 'username': row['SK'], 'location': ''})
         else:
-            # TODO: Add Remaining Data Attributes
             users_table.put_item(
-                Item={'username': row['PK'].lower(), 'date_of_birth': row['DOB']})
+                Item={'username': row['PK'].lower(), 'register_time': row['SK'],
+                      'date_of_birth': row['DOB'], 'first_name': row['firstName'],
+                      'gender': row['Gender'], 'grad_date': row['Grad_date'],
+                      'last_name': row['lastName'], 'major': row['Major'],
+                      'minor': row['Minor']})
+
+    print("Migration Done! :-)")
