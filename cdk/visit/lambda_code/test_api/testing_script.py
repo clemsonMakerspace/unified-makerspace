b5ab2223f3f078b078299ffb32b6a7b81924f7b8
DillonRanwala <dranwal@clemson.edu>
2022-12-02 00:03:36 -0500

    test(cdk/visit): adds API testing lambda and python script for later integration with pipeline and canary testing.
    Problem:
    
    In order to increase the amount of automated testing, we can verify our
    API Gateway, Lambda, and Cloudfront services are functional with API
    test scripts. We need to be able to automatically run these tests
    whenever a new change is pushed and run tests continuously as a canary.
    
    Solution:
    
    This change adds a "TestAPILambda" function to the visit stack and
    creates a testing_script.py. These files run the same tests, but the
    lambda to be used in a canary test (Eventbridge trigger that can run the
    lambda on some set timeframe) while the python script will be called in
    a CodePipeline step. The tests will curl the proper frontend, call the
    visit/register endpoints with dummy data (CANARY_TEST and PIPELINE_TEST
    prefixes for the usernames), and verify that each endpoint is
    functional. Note that this commit only adds the code to the
    repo/Cloudformation. Later commits/changes will set up the tests to
    actually run.
    
    Testing:
    
    The lambda and script were added to Dev visit stacks and CodePipelines
    and displayed the correct behavior for situations where it should both
    succeed and fail.
    
    Issue:
    N/A, No open issue.

---
+++ cdk/visit/lambda_code/test_api/testing_script.py
@@ -0,0 +1,82 @@
+
+import os
+import logging
+from botocore.vendored import requests
+import boto3
+from datetime import datetime
+import json
+import urllib3
+import time
+
+env = os.environ["ENV"]
+
+http = urllib3.PoolManager()  
+
+# Setting up endpoints based on stage
+frontend_url = ""
+api_url = ""
+
+if env == "Beta":
+    frontend_url = "https://beta-visit.cumaker.space/"
+    api_url = "https://beta-api.cumaker.space/"
+elif env == "Prod":
+    frontend_url = "https://visit.cumaker.space/"
+    api_url = "https://api.cumaker.space/"
+else:
+    raise Exception("Couldn't find Stage")
+
+
+    # Simulates "curl <makerspace_frontend_url> | grep Makerspace Visitor Console" command
+frontend_response = http.request('GET', str(frontend_url))
+
+if frontend_response.status != 200:
+    raise Exception("Front End Curl Failed")
+
+
+if frontend_response.data.find(b"Makerspace Visitor Console") == -1:
+   raise Exception("HTML from Front End Error")
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y_%H:%M:%S")
+
+unix_timestamp_for_ttl = int(time.time()+120) # Triggers ttl removal 2 minutes in future 
+
+# testing visit api endpoint
+visit_data = {"username":"PIPELINE_TEST_"+dt_string,"location":"Watt","tool":"Visiting","ttl_expiration":(unix_timestamp_for_ttl)}
+visit_data = json.dumps(visit_data)
+
+visit_data_unregistered = {"username":"PIPELINE_TEST_UNREGISTERED"+dt_string,"location":"Watt","tool":"Visiting","ttl_expiration":(unix_timestamp_for_ttl)}
+visit_data_unregistered  = json.dumps(visit_data_unregistered )
+
+visit_response = http.request('POST', str(api_url)+"visit",body=visit_data)
+visit_response_unregistered = http.request('POST', str(api_url)+"visit",body=visit_data_unregistered)
+
+if visit_response.status != 200 or visit_response_unregistered.status != 200:
+    raise Exception("Visit API Call Failed")
+
+# testing register api endpoint
+register_data = {
+    "username": "PIPELINE_TEST_"+dt_string,
+    "firstName": "TEST",
+    "lastName": "USER",
+    "Gender": "Male",
+    "DOB": "01/01/2000",
+    "UserPosition": "Undergraduate Student",
+    "GradSemester": "Fall",
+    "GradYear": "2023",
+    "Major": ["Mathematical Sciences"],
+    "Minor": ["Business Administration"],
+    "ttl_expiration":(unix_timestamp_for_ttl)
+}
+
+register_data = json.dumps(register_data)
+
+reg_response = http.request('POST', str(api_url)+"register",body=register_data)
+
+if reg_response.status != 200: 
+     raise Exception("Register API Call Failed")
+
+
+print(visit_response.status == 200 and reg_response.status== 200 and frontend_response.status==200)
+
+
