ada8daa14ebe98b6c237bd348363b045112dff5b
DillonRanwala <dranwal@clemson.edu>
2023-03-08 22:00:41 -0500

    feat(cdk): add S3 bucket for logs to Pipeline.
    Problem:
    Makerspace's Logs are currently stored in Google Sheets. This data has
    useful information that can be used in Quicksight, but there is not a
    simple way to connect Quicksight to Google Sheets.
    
    Solution:
    By creating this S3 bucket, we can store the Logs as csv files in our
    AWS ecosystem. Quicksight can create a dataset from S3, so this Log data
    can be visualized.
    
    Testing:
    Dev environment successfully deployed bucket with no external issues.
    
    Issue:
    N/A, No open issue.

---
+++ cdk/log_storage/__init__.py
@@ -0,0 +1,23 @@
+from aws_cdk import (
+    core,
+    aws_s3
+)
+
+class LogStorage(core.Stack):
+    def __init__(self, scope: core.Construct,
+                 stage: str, *, env: core.Environment):  
+              
+        super().__init__(scope, f'LogStorage-{stage}', env=env)
+
+
+        self.s3_log_bucket()
+
+
+        
+    def s3_log_bucket(self):
+        self.log_bucket = aws_s3.Bucket(self, 'quicksight-log-data',
+                        block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
+                        encryption=aws_s3.BucketEncryption.S3_MANAGED,
+                        versioned=True,
+                        removal_policy=core.RemovalPolicy.DESTROY)
+
