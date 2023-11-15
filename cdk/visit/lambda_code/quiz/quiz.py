81c45f2986eb89714142ff57e05e380c76db49b0
Lucas Boyer <lrboyer@clemson.edu>
2023-11-15 14:55:01 -0500

    added logic for add_quiz_info. also fixed bugs with setting tables

--- cdk/visit/lambda_code/quiz/quiz.py
+++ cdk/visit/lambda_code/quiz/quiz.py
@@ -24,16 +24,47 @@ class QuizFunction():
         self.QUIZ_LIST_TABLE_NAME = os.environ["QUIZ_LIST_TABLE_NAME"]
         if quiz_list_table is None:
             dynamodbresource = boto3.resource('dynamodb')
-            self.users = dynamodbresource.Table(self.QUIZ_LIST_TABLE_NAME)
+            self.quiz_list = dynamodbresource.Table(self.QUIZ_LIST_TABLE_NAME)
         else:
             self.quiz_list = quiz_list_table
 
         self.QUIZ_PROGRESS_TABLE_NAME = os.environ["QUIZ_PROGRESS_TABLE_NAME"]
         if quiz_progress_table is None:
             dynamodbresource = boto3.resource('dynamodb')
-            self.users = dynamodbresource.Table(self.QUIZ_PROGRESS_TABLE_NAME)
+            self.quiz_progress = dynamodbresource.Table(
+                self.QUIZ_PROGRESS_TABLE_NAME)
         else:
             self.quiz_progress = quiz_progress_table
+
+    def does_quiz_exist(self, quiz_id):
+        """
+            true if the quiz is in the quiz list
+        """
+        quiz_list_response = self.quiz_list.query(
+            KeyConditionExpression=Key('quiz_id').eq(quiz_id)
+        )
+        return quiz_list_response['Count'] != 0
+
+    def get_username(self, email):
+        userName = email.split('@')
+
+        return userName[0]
+
+    def get_quiz_state(self, score):
+        """
+            input: a string like of the quiz score "9 / 10" or "3 / 3"
+            return: 1 if all questions are correct and a 0 if otherwise
+        """
+        
+        score = score.split('/')
+
+        userScore = int(score[0].strip())
+        totalScore = int(score[1].strip())
+
+        if userScore == totalScore:
+            return 1
+        else:
+            return 0
 
     def add_quiz_info(self, quiz_info):
         """
@@ -44,7 +75,34 @@ class QuizFunction():
                 3. Insert quiz_info into quiz_progress_table
         """
 
-        return None
+        if not self.does_quiz_exist(quiz_info['quiz_id']):
+            quiz_list_response = self.quiz_list.put_item(
+                Item={
+                    'quiz_id': quiz_info['quiz_id']
+                }
+            )
+
+        timestamp = int(time.time())
+        username = self.get_username(quiz_info['email'])
+        state = self.get_quiz_state(quiz_info['score'])
+
+        # dict for entry into the quiz_progress table
+        quiz_progress_item = {
+            'quiz_id': quiz_info['quiz_id'],
+            'username': username,
+            'timestamp': timestamp,
+            'state': state
+        }
+        
+        # if the json is from a test request it will have this ttl attribute
+        if "last_updated" in quiz_info:
+            quiz_progress_item['last_updated'] = {"N":str(quiz_info['last_updated'])}
+
+        quiz_progress_table_response = self.quiz_progress.put_item(
+            Item=quiz_progress_item
+        )
+
+        return quiz_progress_table_response['ResponseMetadata']['HTTPStatusCode']
 
     def handle_quiz_request(self, request, context):
         HEADERS = {
@@ -62,7 +120,7 @@ class QuizFunction():
                 })
             }
 
-        # Get all of the user information from the json file
+        # Get all of the quiz information from the json file
         quiz_info = json.loads(request["body"])
         # Call Function
         response = self.add_quiz_info(quiz_info)
