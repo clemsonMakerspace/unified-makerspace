73193b172a41384ebf99322e5c0bcb73df2a23b3
Sol <sollesesne@gmail.com>
2023-12-14 11:59:02 -0500

    Configure Amplify dynamically based on exact match of environment URL

--- site/visitor-console/src/config/getAmplifyConfig.tsx
+++ site/visitor-console/src/config/getAmplifyConfig.tsx
@@ -8,12 +8,12 @@ interface AmplifyConfig {
 export function getAmplifyConfig(): AmplifyConfig {
   const hostname = window.location.hostname;
 
-  if (hostname.includes("visit.cumaker.space")) {
+  if (hostname === "visit.cumaker.space") {
     return {
       userPoolId: "us-east-1_TQ9FwRJ44",
       userPoolClientId: "58lututlkaggp6h8tu9oauqp5p",
     };
-  } else if (hostname.includes("beta-visit.cumaker.space")) {
+  } else if (hostname === "beta-visit.cumaker.space") {
     return {
       userPoolId: "us-east-1_FXu4qDv8B",
       userPoolClientId: "5rcrlv4el312ht5gos31v0u0n6",
